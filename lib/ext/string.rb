a28d61217f7eb8a707275090ebd0e152649e9728
Whiletruedoend <whiletruedoend@protonmail.com>
2021-01-14 13:43:59 +0300

    fix

--- lib/ext/string.rb
+++ lib/ext/string.rb
@@ -1,5 +1,5 @@
 class String
   def replace_html_to_tg_markdown
-    self.gsub("<p>", "").gsub("</p>", "").gsub("<strong>", "<b>").gsub("</strong>", "</b>")#.gsub("_", '\\_').gsub("*", '\\*')#.gsub("<code>", "`").gsub("</code>", "`")#.gsub("<b>", "*").gsub("</b>", "*").gsub("<em>", "_").gsub("</em>", "_").gsub("<h1>", "").gsub("</h1>", "").gsub("<h2>", "").gsub("</h2>", "").gsub("<h3>", "").gsub("</h3>", "").gsub("<h4>", "").gsub("</h4>", "").gsub("</h4>", "").gsub("<a href=\"", '').gsub("</a>", "").gsub("<ul>", "").gsub("</ul>", "").gsub("<li>", "\\*").gsub("</li>", "").gsub("`", "\\`")
+    self.gsub("<p>", "").gsub("</p>", "").gsub("<strong>", "<b>").gsub("</strong>", "</b>").gsub("<li>", "* ").gsub("</li>", "").gsub("\n\n<ul>", "<code>").gsub("\n</ul>", "</code>")#.gsub("_", '\\_').gsub("*", '\\*')#.gsub("<code>", "`").gsub("</code>", "`")#.gsub("<b>", "*").gsub("</b>", "*").gsub("<em>", "_").gsub("</em>", "_").gsub("<h1>", "").gsub("</h1>", "").gsub("<h2>", "").gsub("</h2>", "").gsub("<h3>", "").gsub("</h3>", "").gsub("<h4>", "").gsub("</h4>", "").gsub("</h4>", "").gsub("<a href=\"", '').gsub("</a>", "").gsub("`", "\\`")
   end
 end
