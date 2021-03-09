1695153b9f6d900334e7104c0c579a204cec878b
Whiletruedoend <whiletruedoend@protonmail.com>
2021-03-09 21:30:14 +0300

    v 0.01 (See update_log.md)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
   def markdown(text)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-                                       no_intra_emphasis: true,
-                                       fenced_code_blocks: true,
-                                       disable_indented_code_blocks: true,
-                                       autolink: true,
-                                       tables: true,
-                                       underline: true,
-                                       highlight: true
-    )
-    markdown.render(text).html_safe
+    renderer = Redcarpet::Render::HTML.new(hard_wrap: true,
+                                           no_intra_emphasis: true,
+                                           fenced_code_blocks: true,
+                                           disable_indented_code_blocks: true,
+                                           autolink: true,
+                                           tables: true,
+                                           underline: true,
+                                           highlight: true)
+    redcarpet = Redcarpet::Markdown.new(renderer)
+    redcarpet.render(text).html_safe
   end
 end
