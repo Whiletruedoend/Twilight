973969d9b45b0573d8d35e78687dea8bf9a6900e
Whiletruedoend <Whiletruedoend@protonmail.com>
2021-10-23 15:47:23 +0300

    Readmore.js && yarn upgrade

---
+++ app/assets/javascripts/readmore.js
@@ -0,0 +1,7 @@
+//= require jquery3
+//= require readmore-js
+
+$('article.feed div.text').readmore({
+    moreLink: '<a class="readmore" href="#">Read more</a>',
+    lessLink: '<a class="readmore" href="#">Hide</a>'
+});
