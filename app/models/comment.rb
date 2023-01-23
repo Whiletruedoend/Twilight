7e87bccc739f296cccd27ba58c24bb0698931bb8
Whiletruedoend <Whiletruedoend@protonmail.com>
2023-01-23 21:20:44 +0300

    Refractoring

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,6 +6,14 @@ class Comment < ApplicationRecord
   belongs_to :platform_user, optional: true # Platform comment
   has_many_attached :attachments
 
+  validate :text_or_attachments
+
+  def text_or_attachments
+    if text.empty? && !has_attachments
+      errors.add(:not_found, "Text and attachments cannot be empty!")
+    end
+  end
+
   def username
     if platform_user.present?
       identifier = platform_user.identifier
