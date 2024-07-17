3d35690e487094285b606b37389870a952574a54
Whiletruedoend <whiletruedoend@protonmail.com>
2024-07-17 22:50:50 +0300

    Matrix update fixes

--- app/services/platform/update_matrix_posts.rb
+++ app/services/platform/update_matrix_posts.rb
@@ -24,13 +24,23 @@ class Platform::UpdateMatrixPosts
     platform_posts = @post.platform_posts.where(platform: Platform.where(title: 'matrix'))
     need_delete_attachments = false
 
+    platform_posts.joins(:content).where(contents: { has_attachments: true }).each do |platform_post|
+      if platform_post.identifier.is_a?(Hash) && platform_post.identifier.dig('options', 'onlylink')
+        room_id = platform_post[:identifier]['room_id']
+        event_id = platform_post[:identifier]['event_id']
+        edit_mx_onlylink_post(platform_post, room_id, event_id)
+        next
+      end
+    end
+
     if @deleted_attachments.present?
       attachments = @deleted_attachments.to_unsafe_h
       del_att = attachments.select { |val| attachments[val] == '0' }
       need_delete_attachments = true if del_att.any?
 
       platform_posts.joins(:content).where(contents: { has_attachments: true }).each do |platform_post|
-        next if platform_post.identifier[0].dig('options', 'onlylink') # array
+        next if platform_post.identifier.is_a?(Array) && platform_post.identifier[0].dig('options', 'onlylink')
+        next if platform_post.identifier.is_a?(Hash) && platform_post.identifier.dig('options', 'onlylink')
 
         matrix_token = platform_post.channel.token
         server = platform_post.channel.options['server']
@@ -76,11 +86,17 @@ class Platform::UpdateMatrixPosts
     text = text.replace_html_to_mx_markdown if text.present?
 
     platform_posts.joins(:content).where(contents: { has_attachments: false }).each do |platform_post|
-      next if platform_post.identifier.dig('options', 'onlylink') # not array
+      room_id = platform_post[:identifier]['room_id']
+      event_id = platform_post[:identifier]['event_id']
+
+      if platform_post.identifier.dig('options', 'onlylink') # not array
+        edit_mx_onlylink_post(platform_post, room_id, event_id)
+        next
+      end
 
       matrix_token = platform_post.channel.token
       server = platform_post.channel.options['server']
-      method = "rooms/#{platform_post[:identifier]['room_id']}/send/m.room.message"
+      method = "rooms/#{room_id}/send/m.room.message"
       data = {
         msgtype: 'm.text',
         format: 'org.matrix.custom.html',
@@ -93,7 +109,7 @@ class Platform::UpdateMatrixPosts
           formatted_body: text
         },
         'm.relates_to': {
-          event_id: platform_post[:identifier]['event_id'],
+          event_id: event_id,
           rel_type: 'm.replace'
         }
       }
@@ -139,5 +155,32 @@ class Platform::UpdateMatrixPosts
       blob_id = ActiveStorage::Blob.find_signed(attachment[0])&.id # may deleted in tg update
       @post.content_attachments.find_by(blob_id: blob_id).purge if blob_id.present? && attachment[1] == '0'
     end
+  end
+
+  def edit_mx_onlylink_post(platform_post, room_id, event_id)
+    post_link = "#{@base_url}/posts/#{@post.id}"
+    full_post_link = "<a href=\"#{post_link}\">#{post_link}</a>"
+    text = @post.title.present? ? "<b>#{@post.title}</b><br>#{full_post_link}" : full_post_link.to_s
+
+    matrix_token = platform_post.channel.token
+    server = platform_post.channel.options['server']
+    method = "rooms/#{room_id}/send/m.room.message"
+    data = {
+      msgtype: 'm.text',
+      format: 'org.matrix.custom.html',
+      body: text,
+      formatted_body: text,
+      'm.new_content': {
+        msgtype: 'm.text',
+        format: 'org.matrix.custom.html',
+        body: text,
+        formatted_body: text
+      },
+      'm.relates_to': {
+        event_id: event_id,
+        rel_type: 'm.replace'
+      }
+    }
+    Matrix.post(server, matrix_token, method, data)
   end
 end
