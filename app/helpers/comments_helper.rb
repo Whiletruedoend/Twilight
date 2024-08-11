f3661cb2033bbda7e32c073a8b2be6f681dee7a1
Whiletruedoend <whiletruedoend@protonmail.com>
2024-08-11 23:27:19 +0300

    Stylesheet improvements

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -7,4 +7,25 @@ module CommentsHelper
           (nested_comments.size > 0 ? content_tag(:div, comments_tree_for(nested_comments, current_user), class: "replies") : nil)
     end.join.html_safe
   end
+
+  def allow_update(comment, current_user)
+    return false if !current_user.present? || comment.identifier&.has_key?("is_deleted")
+    return true if current_user.is_admin
+
+    comment.user.present? && (comment.user == current_user)
+  end
+
+  def allow_destroy(comment, current_user)
+    return false if !current_user.present? || comment.identifier&.has_key?("is_deleted")
+    return true if current_user.is_admin
+
+    comment.user.present? && (comment.user == current_user)
+  end
+
+  def allow_comment(comment, current_user)
+    return false if !current_user.present? || comment.identifier&.has_key?("is_deleted")
+    return true if current_user.is_admin
+
+    true
+  end
 end
